e51123fb2945e7d3edbd16ec4df2812cea14c5fe
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-07-18 15:27:50 +0200

    Sidebar and Home components done

--- sharesmile_frontend/src/components/Sidebar.jsx
+++ sharesmile_frontend/src/components/Sidebar.jsx
@@ -1,9 +1,77 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeSmileFill } from 'react-icons/ri';
+import { BsArrowRightCircleFill } from 'react-icons/bs';
 
-const Sidebar = () => {
+import Logo from '../Assets/Logo/Logo1.png';
+
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-md text-gray-500 hover:text-white transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 text-md text-white font-extrabold border-r-2 border-white transition-all duration-200 ease-in-out capitalize';
+
+const categories = [
+  {name: 'Animals'},
+  {name: 'Wallpapers'},
+  {name: 'Art'},
+  {name: 'Places'},
+  {name: 'Food'},
+  {name: 'Gaming'},
+  {name: 'Memes'},
+  {name: 'Coding'},
+  {name: 'Other'},
+]
+
+const Sidebar = ({ user, closeToggle }) => {
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  }
   return (
-    <div>
-        Sidebar
+    <div className='flex flex-col justify-between bg-gray-800 h-full overflow-y-scroll min-w-210 hide-scrollbar'>
+        <div className='flex flex-col'>
+          <Link
+            to="/"
+            onClick={handleCloseSidebar}
+          >
+            <div className='flex justify-center items-center mb-4'>
+              <div className='p-1 my-2 bg-gray-500 rounded-[75%]'>
+                <img src={Logo} width="60px" alt="Logo" />
+              </div>
+              <div className='text-[1.5em] ml-[5px] font-[Fredoka_One, cursive]'>
+                <h1><span className='text-white'>Share</span><span className='text-gray-400'>Smile</span></h1>
+              </div>
+            </div>
+          </Link>
+          <div className='flex flex-col gap-5'>
+            <NavLink
+              to="/"
+              className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+              onClick={handleCloseSidebar}
+            >
+              <RiHomeSmileFill className='text-white text-3xl'/>
+              <span>Home</span>
+            </NavLink>
+            <h3 className='mt-2 px-5 text-base text-gray-400 2xl:text-md border-b-2 border-white'>Discover categories</h3>
+            {categories.slice(0, categories.length - 1).map((category) => (
+              <NavLink
+                to={`/category/${category.name}`}
+                className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+                onClick={handleCloseSidebar}
+                key={category.name}
+              >
+                {category.name}
+              </NavLink>
+            ))}
+          </div>
+        </div>
+        {user && (
+          <Link
+            to={`user-profile/${user._id}`}
+            className='flex mt-5 my-3 gap-2 p-2 items-center bg-gray-700 rounded-lg shadow-lg text-white mx-3'
+            onClick={handleCloseSidebar}
+          >
+            <img src={user.image} className='w-10 h-10 rounded-full' alt="user-profile" />
+            <p>{user.userName}</p>
+          </Link>
+        )}
     </div>
   )
 }
--- sharesmile_frontend/src/container/Home.jsx
+++ sharesmile_frontend/src/container/Home.jsx
@@ -13,37 +13,53 @@ import Logo from '../Assets/Logo/Logo1.png'
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState()
+  const scrollRef = useRef(null);
+  
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
-
+  
   useEffect(() => {
     const query = userQuery(userInfo?.sub);
     client.fetch(query).then((data) => {
         setUser(data[0]);
       });
   }, [])
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0)
+  }, [])
+  
   
 
   return (
       <div className='flex bg-gray-700 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
         <div className='hidden md:flex h-screen flex-initial'>
-          <Sidebar />
+          <Sidebar user={user && user} />
         </div>
         <div className='flex md:hidden flex-row'>
-          <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)}/>
-          <Link to="/">
-            <img src={Logo} alt="Logo" className='w-10 p-0.5 bg-gray-500 rounded-[75%]' />
-          </Link>
-          <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt="user-pic" className='w-9 h-9 rounded-full' />
-          </Link>
-        </div>
+          <div className='p-2 w-full flex flex-row justify-between items-center bg-gray-800 shadow-md'>
+            <Link to="/">
+              <img src={Logo} alt="Logo" className='w-10 p-0.5 bg-gray-500 rounded-[75%]' />
+            </Link>
+            <HiMenu fontSize={40} className='cursor-pointer text-white mr-[9em]' onClick={() => setToggleSidebar(true)}/>
+            <Link to={`user-profile/${user?._id}`}>
+              <img src={user?.image} alt="user-pic" className='w-9 h-9 rounded-full p-0.5 bg-gray-500' />
+            </Link>
+            </div>
         {toggleSidebar && (
           <div className='fixed w-4/5 bg-gray-500 h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
             <div className='absolute w-full flex justify-end items-center p-2'>
-              <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)}/>
+              <AiFillCloseCircle fontSize={30} className='cursor-pointer text-white' onClick={() => setToggleSidebar(false)}/>
             </div>
+            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
           </div>
         )}
+          </div>
+        <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+          <Routes>
+            <Route path="/user-profile/:userId" element={<UserProfile />} />
+            <Route path="/*" element={<Pins user={user && user} />} />
+          </Routes>
+        </div>
       </div>
   )
 }
